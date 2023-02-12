a046b74555564a02ad5791c3f4ddcf77a592e30a
Muhammet ŞAFAK <info@muhammetsafak.com.tr>
2023-02-12 06:32:52 +0300

    Yorum satırı kaldırıldı

--- assets/js/alert-notify.js
+++ assets/js/alert-notify.js
@@ -81,8 +81,7 @@
             const date = new Date();
             date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
             const expires = "expires=" + date.toUTCString();
-            //document.cookie =
-                name + "=" + value + ";" + expires + ";path=/;Secure";
+            document.cookie = name + "=" + value + ";" + expires + ";path=/;Secure";
         }
 
         function bannerHide()
